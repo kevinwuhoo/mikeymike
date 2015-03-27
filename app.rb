a664a24a9d66014e880ffbe11c9aed834fef636f
Kevin Wu <me@kevinformatics.com>
2015-03-27 14:04:55 -0700

    pie in milanos

--- app.rb
+++ app.rb
@@ -19,7 +19,6 @@ get '/images/:category' do
     'http://wac.9ebf.edgecastcdn.net/809EBF/ec-origin.boston.barstoolsports.com/files/2011/11/cookies_top_milano.jpg',
     'http://cdn1-blog.hwtm.com/wp-content/uploads/2010/10/candycane_mintmilanos_2.jpg',
     'http://s3.amazonaws.com/bzzagent-bzzscapes-prod/hmmmm-mint-milano-lrg.png',
-    'http://archive.delawareonline.com/blogs/secondhelpings/uploaded_images/mattmarden-799425.JPG',
     'https://bootsandcatsblog.files.wordpress.com/2013/04/milano-cookies.jpg'
   ]
 
